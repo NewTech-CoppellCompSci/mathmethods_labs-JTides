b8e14053facaf17c618116fb4e3fca4271247c06
JTides <89484813+JTides@users.noreply.github.com>
2022-02-22 14:12:27 -0600

    submit

--- src/labs/Lab01.java
+++ src/labs/Lab01.java
@@ -1,6 +1,6 @@
 package labs;
 
-
+import java.util.Scanner;
 
 public class Lab01 {
 
@@ -17,6 +17,12 @@ public class Lab01 {
 	
 	public static void problem01() {
 		
+		Scanner inKey = new Scanner(System.in);
+		System.out.println("Choose a positive integer");
+		int userNum = inKey.nextInt();
+		System.out.println("Choose a positive integer");
+		int userNum2 = inKey.nextInt();
+		System.out.println(userNum + "^" + userNum2 + " " + Math.pow(userNum, userNum2));
 		
 		
 	}
@@ -25,8 +31,10 @@ public class Lab01 {
 	
 	
 	public static void problem02() {
-		
-		
+		Scanner inKey = new Scanner(System.in);
+		System.out.println("Choose a positive integer");
+		int userNum = inKey.nextInt();
+		System.out.println("The square root of " + userNum + " is " + Math.sqrt(userNum));
 		
 	}
 
@@ -34,8 +42,12 @@ public class Lab01 {
 	
 
 	public static void problem03() {
-		
-		
+		Scanner inKey = new Scanner(System.in);
+		System.out.println("What is the side A of a right triangle?");
+		int userNum = inKey.nextInt();
+		System.out.println("What is the side B of a right triangle?");
+		int userNum2 = inKey.nextInt();
+		System.out.println("Hyptoneuse = " + Math.sqrt(userNum, 2)+(Math.pow(userNum2)));
 		
 
 	}
@@ -44,8 +56,27 @@ public class Lab01 {
 	
 	
 	public static void problem04() {
-		
-		
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
+		Scanner inKey1 = new Scanner(System.in);
+		int userNum = 1;
+		boolean not0 = true;
+		while (not0) {
+			System.out.println("Choose an integer, choose 0 when you're done");
+			userNum = inKey1.nextInt();
+			if (userNum ==0) {
+				not0=false;
+			}
+			else {
+				if (userNum > max) {
+				}
+				else if (userNum < min) {
+					min = userNum;
+				}
+			}
+		}
+		System.out.println(max);
+		System.out.println(min + "\n");
 		
 	}
 	
